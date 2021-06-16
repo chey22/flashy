7fcaf4dbd8633a38c61df6d0ba82d095c23ed591
Cheyenne Mangum <cheyennemangum@gmail.com>
2021-06-16 16:50:15 -0500

    step 6.2 - set up articles/show page; added resources method to routes; add link to each article from list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,12 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  # The show action calls Article.find with the ID captured by the route parameter.
+  # The returned article is stored in the @article instance variable, so it is accessible by the view.
+  # By default, the show action will render app/views/articles/show.html.erb
+  # Now go create that file.
+  def show
+    @article = Article.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # # view that lists all articles in our database
+  # get "/articles", to: "articles#index"
+
+  # # view that shows the title and body of a single article
+  #   # A route parameter of :id captures a segment of the request's path,
+  #   # and puts that value into the params Hash,
+  #   # which is accessible by the controller action.
+
+  #   # For example, when handling a request like GET http://localhost:3000/articles/1,
+  #   # 1 would be captured as the value for :id,
+  #   # which would then be accessible as params[:id] in the show action of ArticlesController
+  # get "/articles/:id", to: "articles#show"
+
+  
+  # method that maps all of the conventional routes for a collection of resources, such as articles
+    # also makes the above 2 get methods obsolete
+  # also sets up URL and path helper methods that we can use to keep our code from depending on a specific route configuration.
+  # The values in the "Prefix" column (from running bin/rails routes) plus a suffix of _url or _path form the names of these helpers.
+  # For example, the article_path helper returns "/articles/#{article.id}" when given an article
+  resources :articles
 end
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
