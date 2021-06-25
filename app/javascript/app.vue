8e6e429f7c8e53ade7eba5771c0f434f7da9e2c0
Cheyenne Mangum <cheyennemangum@gmail.com>
2021-06-25 03:22:44 -0500

    installed Vue & Turbolinks

---
+++ app/javascript/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- <div id="app">
+    <p>{{ message }}</p>
+  </div> -->
+  <div id='flashy'>
+    {{message}}
+    <app></app>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+    console.log(message)
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
